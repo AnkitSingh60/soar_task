bd4ddce2ac9ebecb6d44d56b6174cfa44331be6d
Ankit Singh <ankitanwar730@gmail.com>
2025-01-01 00:59:03 +0530

    feat: add master card icon component

---
+++ src/components/icons/MasterCardShadowIcon.tsx
@@ -0,0 +1,21 @@
+type Props = {
+  className?: string;
+};
+
+export default function MasterCardShadowIcon(props: Props) {
+  return (
+    <>
+      <svg
+        className={props.className}
+        width="44"
+        height="30"
+        viewBox="0 0 44 30"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle cx="15" cy="15" r="15" fill="white" fillOpacity="0.5" />
+        <circle cx="29" cy="15" r="15" fill="white" fillOpacity="0.5" />
+      </svg>
+    </>
+  );
+}
